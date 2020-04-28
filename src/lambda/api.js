1c84ae16d4bc85af81af520c19c8607c40cef606
Florent Merlat <f.merlat@choosit.com>
2020-04-28 09:28:42 +0200

    .env + routes

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const serverless = require('serverless-http');
-const debug = require('debug')('api')
+const debug = require('debug')('api');
+const axios = require('axios').default;
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,21 @@ router.get('/hello', (req, res) => {
     message: "Hello world"
   });
 })
+router.get('/links/:linkId', (req, res) => {
+  // db.collection("dynamic_links")
+  //   .get()
+  //   .then((snapshot) => {
+  //     let docs = [];
+  //     snapshot.forEach((doc) => docs.push(doc.data()));
+  //     res.json(docs);
+  //   });
+});
+router.get('/links', (req, res) => {
+});
+router.delete('/links/:linkId', (req, res) => {
+});
+router.post('/links', (req, res) => {
+});
 
 app.use(process.env.API_BASE_PATH, router);
 
